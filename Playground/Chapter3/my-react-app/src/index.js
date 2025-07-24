11416911c5059d9d1dcaa5a12af7e83cda56b383
Bobby Ly <bobby.ly@check24.de>
2025-07-24 14:12:02 +0200

    complete Chapter "React einrichten"

--- Playground/Chapter3/my-react-app/src/index.js
+++ Playground/Chapter3/my-react-app/src/index.js
@@ -4,8 +4,9 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>Hello!</div>
-  </React.StrictMode>
+    <div>Hello World!</div>
+</React.StrictMode>
 );
+
 
 
